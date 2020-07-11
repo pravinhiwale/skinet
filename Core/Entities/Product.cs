6d9dc48c15bd7d08de8da5d9748ced0b85726afb
Pravin Hiwale <Pravin.Hiwale@yardi.com>
2020-07-11 12:22:51 +1000

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,61 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name  { get; set; }
+
+    }
+}
+
+/*
+We don't query our database directly in asp.net core.
+We typically go through or via an Object Relational mapper that abstracts the database away from our code 
+and this comes with several advantages it's certainly one of the advantages is it allows us to very 
+easily swap the database provided that we're using
+And like I say we're using sql lite for development but we won't be using simple sqllite in production
+
+We'll swap it for a production database server such as sql or mysql but because we're going to
+use an Object Relational map this makes it very easy for us to do so and the Object Relational map that
+we're going to use is entity framework the one that Microsoft creates and obviously works very well with dot net core
+
+
+
+
+*/
+
+/*
+OK so now that we've configured our database context (storeContext.cs) what we can look at now is entity framework core migrations
+And this is going to generate some code so that we can scaffold our database and create our database based on the code that we've written so far.
+
+And what this is going to do it's going to take a look in our StoreContext.
+It's going to read from this  public DbSet<Product> Products {get;set;}.
+It's going to see that we've got a DB set property here related to our product entity and it's going
+to generate a table called "Products" based on the name that we specified here, 
+this table's going to contain two columns based on the properties inside our product class
+So we can expect to have an "Id" column as well as a "Name" column 
+and by convention an entity Framework Core is convention based.
+If we specify an "Id" that is an integer and it's called "Id" that it's going to use this field as the primary key of the table
+And because it's an integer it's going to set the column up to auto generates a new id every time 
+a new record or a new product is added to that database and without us needing to do any further configuration 
+this is just what it's going to do by convention
+
+for dot net migration tool use sdk version rather the host version
+check using dotnet --info
+
+i used below
+
+dotnet tool install --global dotnet-ef --version  3.1.1
+dotnet tool install --global dotnet-ef --version  3.1.1
+
+dotnet ef migrations add InitialCreate -o Data/Migrations
+
+after running above you will get error 
+Your startup project 'API' doesn't reference Microsoft.EntityFrameworkCore.Design. This package is required for the Entity Framework Core Tools to work. Ensure your startup project is correct, install the package, and try again.
+
+you just need to add package  Microsoft.EntiryFrameworkCore.Design 
+
+after that update database
+dotnet ef database update
+
+*/
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,55 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        /*
+        Just hover the mouse to StoreContext and press cmd + .  and it help you generating constructor
+        below is the constructor that we need so that we can provide it with some options and the options we'll give it is as is the connection string and then it passes those options
+        up into the base constructor and the base constructor is is what we were just looking at in terms of constuctor you see in metadata of DBContext
+        So we're going to pass those options up to its parent class.
+
+        One Esstential thing we will be doing is we'll tell the DBcontext options what type we're passing up here as well 
+        and will pass in the StoreContext
+
+        */
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+        /*
+        And this is going to be the plurals name of the entity and it doesn't have to be its convention that we're looking at here 
+        but this "products" is gonna be the name of the table that gets created when we generate the code to generate our database
+        
+        */
+    }
+}
+
+/*
+dotnet --info gives you dotnet version info , it should match the Microsoft.EntityFrameworkCore
+
+for first time use we need to add nuget package 
+Microsoft.EntityFrameworkCore  (version 3.1.5 matching with dotnet core) 
+
+for SQL lite
+add Nuget package  Microsoft.EntityFrameworkCore.sqllite
+Do not Add  .sqllite.core
+
+*/
+
+/*
+if you hover mouse over DBContext it will say
+A DbContext instance represents a session with the database and can be used to query and save instances of your entities. DbContext is a combination of the Unit Of Work and Repository patterns.
+
+what this really means is that we're abstracting our database away from our code.
+We we do not directly query our database.
+We use our DBcontext methods to query our database.
+
+Now if we go in right click on DBContext and go to definition then we can see the metadata about the DB context class
+
+And what we're going to need to do for this particular class is we can add some DBContextOptions to this as well 
+nd this allows us to give it a connection string so that it can go ahead and query the correct database
+*/
